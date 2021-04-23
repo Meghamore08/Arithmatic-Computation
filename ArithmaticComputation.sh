f579794e690d39fd411cf49b785524d150492a0f
Megha <meghamoree@gmail.com>
2021-04-23 15:51:21 +0530

    Added:Third Expression

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -4,8 +4,9 @@ echo "Enter the three input a , b and c :"
 read a b c
 
 result1=$(($a + $b * $c))
-result2=$(($a * $b +$c ))
+result2=$(($a * $b + $c))
+result3=$(($c + $a / $b))
 
 echo $result1
 echo $result2
-
+echo $result3
