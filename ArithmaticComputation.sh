712dc2b65268fbfb10fe734c225f76993afea334
Megha <meghamoree@gmail.com>
2021-04-23 18:33:38 +0530

    Added:Read from Dictionary into Array

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -1,13 +1,28 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 
-echo "Enter the three input a , b and c :"
+echo "Enter the three input a, b and c :"
 read a b c
 
 declare -A result
 
-result[1]=$(($a + $b * $c))
-result[2]=$(($a * $b + $c))
-result[3]=$(($c + $a / $b))
-result[4]=$(($a % $b + $c))
+UC2=$(($a + $b * $c))
+UC3=$(($a * $b + $c))
+UC4=$(($c + $a / $b))
+UC5=$(($a % $b + $c))
+
+result[1]=$UC2
+result[2]=$UC3
+result[3]=$UC4
+result[4]=$UC5
+
+echo "Output in array is"
+
+for ((i=1; i<=4; i++))
+do
+UseCases[$i]=${result[$i]}
+echo ${UseCases[$i]}
+done
 
 echo "UseCases" ${result[@]}
+
+
